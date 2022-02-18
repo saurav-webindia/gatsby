6fb7ce64ec052ba769014cf022fcea92df6a8cfd
Saurav Tyagi <50488106+saurav-webindia@users.noreply.github.com>
2022-02-18 12:32:07 +0530

    Update Head.jsx

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -7,14 +7,14 @@ function Head() {
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>JGU - O.P Jindal Global University</title>
-        <link rel="stylesheet" href="css/bootstrap.min.css" />
-        <link rel="stylesheet" href="css/all.min.css" />
+        <link rel="stylesheet" href="/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="/css/all.min.css" />
         <link
           rel="stylesheet"
           href="https://unpkg.com/swiper/swiper-bundle.min.css"
         />
-        <link rel="stylesheet" href="css/custom1.css" />
-        <link rel="stylesheet" href="css/custom2.css" />
+        <link rel="stylesheet" href="/css/custom1.css" />
+        <link rel="stylesheet" href="/css/custom2.css" />
       </head>
     </div>
   );
